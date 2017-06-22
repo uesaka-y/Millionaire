8a30ca9f088fc55f9c7a0bcf37e90a4da632af49
FNDM11 <fndm11@FNDM11-no-MacBook-Air.local>
2017-06-22 10:28:40 +0900

    バリデーション操作

--- theMillionaire/app/models/user.rb
+++ theMillionaire/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
+  validates :user_id, :presence => true, :uniqueness => true
+  validates :name, :presence => true, :uniqueness => true
+  validates :password, :presence => true, :uniqueness => true
+
+
 
 before_create :digest_password
    def self.authenticate(user_id, password)
