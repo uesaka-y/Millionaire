b3b7866a6c5212b78f223b5eb16a1fb343ebd0d1
FNDM57 <h-kawamoto@froide.co.jp>
2017-06-23 11:59:48 +0900

    Age

--- theMillionaire/app/controllers/users_controller.rb
+++ theMillionaire/app/controllers/users_controller.rb
@@ -36,21 +36,24 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
+     @user = User.new(user_params)
 
 
-      if @user.save
-        session[:user_id] = @user.id
-        flash[:notice] = "ログインに成功しました"
-        redirect_to setup_path #ユーザーのメニュー画面へいく
-      else
-        format.html { render :new }
-      end
+       if @user.save
+         session[:user_id] = @user.id
+         flash[:notice] = "ログインに成功しました"
+         if @user.adm
+         redirect_to users_path
+       else
+         redirect_to setup_path
+       end
+       else
+         format.html { render :new }
+       end
 
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+   end
+   # PATCH/PUT /users/1
+   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
